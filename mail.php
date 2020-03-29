5a279b580077644beed5031129f46ab9f1d7c125
moonhanna <mhn97@naver.com>
2020-03-29 20:43:23 +0900

    update mailto

--- mail.php
+++ mail.php
@@ -1,9 +1,9 @@
 <?php
-    $to = "youremail@.com"; // replace this mail with yours
+    $to = "mhn97@naver.com"; // replace this mail with yours
     $from = $_SERVER['PHP_SELF']." ".$_POST["email"];
     $fname = $_POST["name"];
     $email = $_POST["email"];
-    $lname = $_POST["subject"];
+    $subject = $_POST["subject"];
     $headers = "From: $from";
     $message = $_POST["message"];
 
@@ -13,8 +13,5 @@
     $body .= " \n\n\t Subject: ".$subject;
     $body .= " \n\n\t Message: ".$message;
 
-    if(mail($to, $subject, $body, $headers)){
-        echo '<label class="success">Sent your <b>e-mail.</b></label>';
-    }else{
-        echo '<label class="error">Something went wrong! please try again.</label>';
-    }
+    mail($to, $subject, $message);
+?>
